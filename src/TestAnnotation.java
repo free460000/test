4d67e58faec744cd991c34595f0af420cfb67220
free460000 <free46000@126.com>
2014-10-12 14:11:23 +0800

    Update TestAnnotation.java

--- src/TestAnnotation.java
+++ src/TestAnnotation.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface TestAnnotation {
-
+//aaaaaa
 	public String value_test() default "s";
 
 	public String test_test() default "aa";
