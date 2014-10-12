2888365bdb98ddcb1eda9cb6674bf17bfbe7e5b2
free460000 <free46000@126.com>
2014-10-12 14:24:40 +0800

    Update TestAnnotation.java

--- src/TestAnnotation.java
+++ src/TestAnnotation.java
@@ -8,6 +8,7 @@ import java.lang.annotation.Target;
 public @interface TestAnnotation {
 //aaaaaa
 //aaaassss
+//dddffff
 	public String value_test() default "s";
 
 	public String test_test() default "aa";
